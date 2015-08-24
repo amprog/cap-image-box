22aa74ea11ac33627c26bd286d22643b13521ffb
Brian Broderick <bbroderick@americanprogress.org>
2015-08-24 12:14:43 -0400

    rename

--- cap-image-box.php
+++ cap-image-box.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: WordPress Image Box
-Plugin URI: https://github.com/capwebtech/wp-image-box
+Plugin URI: https://github.com/amprog/cap-image-box
 Description: Rewrites the WP image box and caption box, also provides misc image utilities.
 Version: 1.0
 Author: Seth Rubenstein for Center for American Progress
-Author URI: http://sethrubenstein.info
+Author URI: https://github.com/amprog
 */
 $plugin_dir = plugin_dir_path( __FILE__ );
 include $plugin_dir.'/display.php';
