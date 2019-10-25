78327325a050d3ba0f8bd6288d3fe4c040e1c569
Matt J Grant <mgrant@americanprogress.org>
2019-10-25 14:39:24 -0400

    Adding a new way of detecting empty or non-existent "alt" property. - MJG

--- display.php
+++ display.php
@@ -11,7 +11,7 @@ function wpib_image_box_js() {
 	jQuery(document).ready(function($){
 	    $('img.wp-photo').each(function() {
 			if ($(this).data('credit') != undefined) {
-                if ($(this).attr("alt") == undefined) {
+                if ($(this).attr("alt") == undefined || $(this).attr("alt").length == 0) {
                     $(this).attr("alt") = $(this).data('credit');
                 }
 				credit = '<span class="photo-credit">'+$(this).data('credit')+'</span>';
